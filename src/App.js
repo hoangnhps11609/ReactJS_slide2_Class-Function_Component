4f29304bf1477615ed423102df9d7ff427b212b0
Hoang <hoangnhps11609@gmail.com>
2022-04-20 13:48:10 +0700

    'slide2'

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Comment from './components/Comment';
+import FComment from './components/FComment';
+
+
+const comments = [
+  {id: 1, author: 'Hoang1', content:'Hoang 1', timeAgo: '1'},
+  {id: 2, author: 'Hoang2', content:'Hoang 2', timeAgo: '2'},
+  {id: 3, author: 'Hoang3', content:'Hoang 3', timeAgo: '3'},
+  {id: 4, author: 'Hoang4', content:'Hoang 4', timeAgo: '4'},
+]
 
 function App() {
+const contents = comments.map(item => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <FComment key={item.id} author={item.author} content={item.content} timeAgo={item.timeAgo}></FComment> 
+  )
+})
+
+  return (
+    <div className="comments container ui">
+        <h3>Function Conponent</h3>
+        {contents}
+        <hr />
+        <h3>Class Conponent</h3>
+        {
+          comments.length <=0 ? <p>No Comments</p> :
+          comments.map(item =>(
+              <Comment key={item.id} author={item.author} content={item.content} timeAgo={item.timeAgo}></Comment> 
+            )
+          )
+        }
+
+        <hr />
+        {comments.length > 0 && <h3>Total comments: {comments.length}</h3>}
     </div>
   );
 }
---
+++ src/components/Comment.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import faker from 'faker'
+
+class Comment extends Component {
+  constructor(props) {
+    super();
+  }
+
+  render() {
+    return (
+      <div className="ui container comment">
+        <a href="" className="avatar">
+          <img src={faker.image.avatar()} alt="avatar" />
+        </a>
+        <div className="content">
+          <a href="" className="author">
+            {this.props.author}
+          </a>
+          <div className="metadata">
+            <span className="date">{this.props.timeAgo}</span>
+          </div>
+          <div className="text">{this.props.content}</div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Comment;
